38c5de1eab515a7e6850dba58b958982c340ed1d
aliciasavelly <alicia.savelly@gmail.com>
2017-03-14 14:35:58 -0700

    remove store error

--- frontend/sharebnb.jsx
+++ frontend/sharebnb.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import configureStore from './store/store';
-// import Root from './components/root';
-
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import configureStore from './store/store';
+// // import Root from './components/root';
+//
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h2>I am root</h2>, root)
