3cb4e8a72a6571b0ce8b9add48abaa13cc8e6564
aliciasavelly <alicia.savelly@gmail.com>
2017-03-14 14:12:04 -0700

    edit sharebnb frontend file

---
+++ frontend/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = ({ children }) => (
+  <div>
+    <h1>Sharebnb</h1>
+    { children }
+  </div>
+)
+
+export default App;
---
+++ frontend/components/root.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import App from './app';
+
+const Root = ({ store }) => {
+  return(
+    <Provider store={ store }>
+      <Router history={ hashHistory }>
+        <Router path='/' component={ App } >
+
+        </Router>
+      </Router>
+    </Provider>
+  )
+}
