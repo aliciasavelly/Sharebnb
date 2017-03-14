909317877ac2718d8857b36c581e9ac4a310cc6c
aliciasavelly <alicia.savelly@gmail.com>
2017-03-14 15:09:39 -0700

    migrate users table and create users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,38 @@
 class User < ApplicationRecord
+  validates :username, :first_name, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  after_initialize :ensure_session_token
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    return user if user && user.is_password?(password)
+    nil
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 end
--- db/migrate/20170314215842_create_users.rb
+++ db/migrate/20170314215842_create_users.rb
@@ -1,8 +1,15 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
+      t.string :username, null: false
+      t.string :first_name, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
+      t.string :image_url
 
       t.timestamps
     end
+    add_index :users, :username, unique: true
+    add_index :users, :password_digest
   end
 end
