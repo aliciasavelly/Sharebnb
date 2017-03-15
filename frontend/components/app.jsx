c657fc4cfc946d6119575e387612a465892f0b78
aliciasavelly <alicia.savelly@gmail.com>
2017-03-14 22:40:04 -0700

    add first_name to user jbuilder and include welcome message at root

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const App = ({ children }) => (
+const App = ({ children }) => {
+  return (
   <div>
     <h1>Sharebnb</h1>
+    <p>Hi, {currentUser.first_name}! Your username is {currentUser.username}.</p>
     { children }
   </div>
-);
+)};
 
 export default App;
