2bf0e059227f7cd5772fd7a758499e33ba614221
aliciasavelly <alicia.savelly@gmail.com>
2017-03-17 13:29:01 -0700

    fix seed file typos

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,43 +3,43 @@
 
 
 Destination.create!(
-  city: San Francisco,
+  city: "San Francisco",
   image_url: "http://res.cloudinary.com/sharebnb/image/upload/v1489777250/andrew-ruiz-sf-horizontal_fdvnpl.jpg",
   lat: 37.792081,
-  lng: -122.419413
+  lng: -122.419413,
 )
 
 Destination.create!(
-  city: Boston,
+  city: "Boston",
   image_url: "http://res.cloudinary.com/sharebnb/image/upload/v1489777250/andrew-ruiz-sf-horizontal_fdvnpl.jpg",
   lat: 42.357004,
-  lng: -71.062309
+  lng: -71.062309,
 )
 
 Destination.create!(
-  city: New York,
+  city: "New York",
   image_url: "http://res.cloudinary.com/sharebnb/image/upload/v1489777250/andrew-ruiz-sf-horizontal_fdvnpl.jpg",
   lat: 40.777878,
-  lng: -73.937499
+  lng: -73.937499,
 )
 
 Destination.create!(
-  city: Seattle,
+  city: "Seattle",
   image_url: "http://res.cloudinary.com/sharebnb/image/upload/v1489777250/andrew-ruiz-sf-horizontal_fdvnpl.jpg",
   lat: 47.598559,
-  lng: -122.326300
+  lng: -122.326300,
 )
 
 Destination.create!(
-  city: Chicago,
+  city: "Chicago",
   image_url: "http://res.cloudinary.com/sharebnb/image/upload/v1489777250/andrew-ruiz-sf-horizontal_fdvnpl.jpg",
   lat: 41.883979,
-  lng: -87.634669
+  lng: -87.634669,
 )
 
 Destination.create!(
-  city: Detroit,
+  city: "Detroit",
   image_url: "http://res.cloudinary.com/sharebnb/image/upload/v1489777250/andrew-ruiz-sf-horizontal_fdvnpl.jpg",
   lat: 42.336985,
-  lng: -83.054480
+  lng: -83.054480,
 )
