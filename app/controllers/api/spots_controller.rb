1fff5fee0e4f53199fe6c6e4d25e825c894d287a
aliciasavelly <alicia.savelly@gmail.com>
2017-03-18 17:12:19 -0700

    continue writing spots controller

--- app/controllers/api/spots_controller.rb
+++ app/controllers/api/spots_controller.rb
@@ -1,2 +1,34 @@
 class Api::SpotsController < ApplicationController
+  #before_action :require_logged_in, only: [:create]
+
+  def index
+    @spots = (bounds ? Spot.in_bounds(bounds) : Spot.all)
+  end
+
+  def show
+    @spot = Spot.find(params[:id])
+  end
+
+  def create
+    @spot = Spot.create(spot_params)
+    render :show
+  end
+
+  private
+  def spots_params
+    params.require(:spot).permit(
+      :title,
+      :destination_id,
+      :price,
+      :image_url,
+      :host_id,
+      :description,
+      :lat,
+      :lng
+    )
+  end
+
+  def bounds
+    params[:bounds]
+  end
 end
