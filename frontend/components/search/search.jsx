ab5389bbe585bed4a4374b7b187adbfe6edc2991
aliciasavelly <alicia.savelly@gmail.com>
2017-03-20 22:38:04 -0700

    attempt to set up price slider

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import SpotsIndex from './spots_index';
 import SpotsMap from '../spots_map/spots_map';
+import Slider from 'react-rangeslider';
+// import 'react-rangeslider/lib/index.css';
+
 //TODO
 // import SpotMap from './../spot_map/spot_map';
 // import FilterForm from './filter_form';
 
-const Search = ({ spots, requestSpots }) => (
+const Search = ({ spots, requestSpots }) => {
+  return (
   <div className="spots-index-container">
     <div className="spots-index-left">
       <div className="spots-index-filters">
-        insert here
+        price filter here
+        <Slider className="slider" orientation="vertical" defaultValue={20} step={ 5 } />
       </div>
       <SpotsIndex spots={ spots } requestSpots={ requestSpots } />
     </div>
@@ -20,6 +25,7 @@ const Search = ({ spots, requestSpots }) => (
       </div>
     </div>
   </div>
-);
+  )
+};
 
 export default Search;
