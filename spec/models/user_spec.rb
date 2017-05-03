fa822b34e523dabdedc20b0027cc0f353c14eb14
aliciasavelly <alicia.savelly@gmail.com>
2017-05-02 17:10:21 -0700

    begin adding specs and add to spec helper

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:session_token) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_length_of(:password).is_at_least(6) }
+end
