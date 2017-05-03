782722b00d8a0cc676cf2b576fd745a47bfa2c5b
aliciasavelly <alicia.savelly@gmail.com>
2017-05-02 22:03:24 -0700

    work on adding tests to user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:first_name) }
   it { should validate_presence_of(:session_token) }
   it { should validate_presence_of(:password_digest) }
   it { should validate_length_of(:password).is_at_least(6) }
+  it { should have_many(:hosted_spots) }
+  it { should have_many(:trips) }
+  # it { should validate_uniqueness_of(:username) }
 end
