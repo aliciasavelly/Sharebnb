50fe6c5ddffa08feb7191de2ab57af984dfc8ac5
aliciasavelly <alicia.savelly@gmail.com>
2017-05-23 22:06:00 -0700

    add more locations to show up on search and clean up code

--- frontend/components/spots_map/spots_map.jsx
+++ frontend/components/spots_map/spots_map.jsx
@@ -13,8 +13,10 @@ const destinationsList = {"Boston": {center: { lat: 42.357004, lng: -71.062309 }
                           "Washington": {center: { lat: 47.417435, lng: -120.072763 }, zoom: 6},
                           "NYC": {center: { lat: 40.777878, lng: -73.937499 }, zoom: 10},
                           "SF": {center: { lat: 37.777072, lng: -122.447774 }, zoom: 12},
-                          "Massachusetts": {center: { lat: 42.407140, lng: -71.643438}, zoom: 7},
-                          "Illinois": {center: { lat: 40.876589, lng: -88.832609}, zoom: 7}}
+                          "Massachusetts": {center: { lat: 42.407140, lng: -71.643438 }, zoom: 7},
+                          "Illinois": {center: { lat: 40.876589, lng: -88.832609 }, zoom: 7},
+                          "US": {center: { lat: 37.681841, lng: -97.750673 }, zoom: 4},
+                          "United States": {center: { lat: 37.681841, lng: -97.750673 }, zoom: 4}}
 
 class SpotsMap extends React.Component {
 
@@ -39,7 +41,6 @@ class SpotsMap extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    // debugger;
     if(nextProps.filters.letters) {
       if (nextProps.filters.letters !== this.props.filters.letters) {
         this.state = { letters: nextProps.filters.letters };
@@ -60,29 +61,6 @@ class SpotsMap extends React.Component {
     }
     this.MarkerManager.updateMarkers(this.props.spots);
   }
-
-  // componentDidUpdate(nextProps) {
-  //   if(nextProps.filters.letters) {
-  //     if (nextProps.filters.letters !== this.props.filters.letters) {
-  //       debugger;
-  //       this.state = { letters: nextProps.filters.letters };
-  //       let foundLocation = false;
-  //       Object.keys(destinationsList).forEach( key => {
-  //         if(key.toLowerCase().includes(this.state.letters.toLowerCase())) {
-  //           this.map.setCenter(destinationsList[key].center);
-  //           this.map.setZoom(destinationsList[key].zoom);
-  //           foundLocation = true;
-  //         } else {
-  //         }
-  //       });
-  //       if (!foundLocation) {
-  //         this.map.setZoom(2);
-  //       }
-  //       this.MarkerManager.updateMarkers(this.props.spots);
-  //     }
-  //   }
-  //   this.MarkerManager.updateMarkers(this.props.spots);
-  // }
 
   _registerListeners() {
     google.maps.event.addListener(this.map, 'idle', () => {
