3955cc5849a96628433d7164a81b304d3fdad402
aliciasavelly <alicia.savelly@gmail.com>
2017-06-04 16:39:58 -0700

    clean up spot update container

--- frontend/components/spot_create/spot_update_container.js
+++ frontend/components/spot_create/spot_update_container.js
@@ -6,21 +6,14 @@ import { requestDestinations } from '../../actions/destination_actions';
 import { selectAllDestinations } from '../../reducers/selectors';
 
 const mapStateToProps = (state, ownProps) => ({
-    loggedIn: !!state.session.currentUser,
     currentUser: state.session.currentUser,
-    filters: state.filters,
-    lat: ownProps.location.query.lat,
-    lng: ownProps.location.query.lng,
     destinations: selectAllDestinations(state),
     hosted_spots: state.session.currentUser.hosted_spots,
     url_id: ownProps.location.pathname.slice(14)
 });
 
 const mapDispatchToProps = dispatch => ({
-  createSpot: (spot) => dispatch(createSpot(spot)),
   updateSpot: (spotId, data) => dispatch(updateSpot(spotId, data)),
-  deleteSpot: (spotId) => dispatch(deleteSpot(spotId)),
-  updateFilter: (filter, value) => dispatch(updateFilter(filter, value)),
   requestDestinations: () => dispatch(requestDestinations())
 });
 
