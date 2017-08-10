65630b7ddf8522bcf2ffe1bf4369fffc8b37b085
aliciasavelly <alicia.savelly@gmail.com>
2017-08-09 20:12:36 -0700

    clean up gemfile

--- Gemfile
+++ Gemfile
@@ -51,10 +51,10 @@ group :development do
   gem 'spring'
   # gem 'listen'
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
-  gem 'web-console', '>= 3.3.0'
+  # gem 'web-console', '>= 3.3.0'
   gem 'listen', '~> 3.0.5'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  gem 'spring'
+  # gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem "guard", ">= 2.2.2", require: false
   gem "guard-livereload", require: false
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -6,14 +6,14 @@ class Destination < ApplicationRecord
   #   foreign_key: :destination_id,
   #   class_name: :Spot
 
-    def self.locations
-      Destination.select(:city).pluck(:city).uniq
-    end
+  def self.locations
+    Destination.select(:city).pluck(:city).uniq
+  end
 
-    def self.find_by_letters(letters)
-      Destination.select(:city)
-        .where("city ~ ?", letters)
-        .pluck(:city)
-        .uniq
-    end
+  def self.find_by_letters(letters)
+    Destination.select(:city)
+      .where("city ~ ?", letters)
+      .pluck(:city)
+      .uniq
+  end
 end
