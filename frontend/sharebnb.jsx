529b038b9defcd4385ea7a325b0c82cf7101bb96
aliciasavelly <alicia.savelly@gmail.com>
2017-10-26 19:32:39 -0700

    removed random imports in sharebnb.jsx file

--- frontend/sharebnb.jsx
+++ frontend/sharebnb.jsx
@@ -18,35 +18,3 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
 });
-
-
-// import { requestLogin, requestLogout, requestSignup } from './actions/session_actions';
-// window.login = requestLogin;
-// window.logout = requestLogout;
-// window.signup = requestSignup;
-// import { fetchTrips } from './util/trip_api_util';
-// window.fetchTrips = fetchTrips;
-// import { requestDestinations } from './actions/destination_actions';
-// window.requestDestinations = requestDestinations;
-// import { requestTrips } from './actions/trip_actions';
-// window.requestTrips = requestTrips;
-// import { requestListings } from './actions/listings_actions';
-// window.requestListings = requestListings;
-// import { createTrip } from './actions/trip_actions';
-// window.createTrip = createTrip;
-// import { fetchDestinations } from './util/destinations_api_util';
-// import { fetchSpots, fetchSingleSpot } from './util/spot_api_util';
-// import { fetchHostedSpots } from './util/listings_api_util';
-// window.fetchHostedSpots = fetchHostedSpots;
-// window.fetchSpots = fetchSpots;
-// window.fetchSingleSpot = fetchSingleSpot;
-// import { requestSpots, requestSingleSpot, createSpot, updateSpot, deleteSpot } from './actions/spot_actions';
-// window.requestSpots = requestSpots;
-// window.requestSingleSpot = requestSingleSpot;
-// window.createSpot = createSpot;
-// window.updateSpot = updateSpot;
-// window.deleteSpot = deleteSpot;
-// import { fetchDestinations } from './util/destination_api_util';
-// window.fetchDestinations = fetchDestinations;
-// const windowFunctions = { requestLogin, requestLogout, requestSignup }
-// Object.assign(window, windowFunctions);
