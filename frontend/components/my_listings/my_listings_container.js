a739e24627b0b442d1a8a83183dddd958e9c913d
aliciasavelly <alicia.savelly@gmail.com>
2017-10-29 16:00:33 -0700

    clean up my listings container

--- frontend/components/my_listings/my_listings_container.js
+++ frontend/components/my_listings/my_listings_container.js
@@ -1,8 +1,8 @@
+import { selectAllListings } from '../../reducers/selectors';
+import { deleteSpot, requestSingleSpot } from '../../actions/spot_actions';
+import { requestListings } from '../../actions/listings_actions';
 import { connect } from 'react-redux';
 import MyListings from './my_listings';
-import { selectAllListings } from '../../reducers/selectors';
-import { createSpot, updateSpot, deleteSpot, requestSingleSpot } from '../../actions/spot_actions';
-import { requestListings } from '../../actions/listings_actions';
 
 const mapStateToProps = state => ({
   listings: selectAllListings(state),
