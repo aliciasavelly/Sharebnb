a8cc79d21202c74cf6f393b15f3ced4a96da69cc
aliciasavelly <alicia.savelly@gmail.com>
2017-10-29 16:11:05 -0700

    add listings prop back into listings container

--- frontend/components/my_listings/my_listings_container.js
+++ frontend/components/my_listings/my_listings_container.js
@@ -1,11 +1,11 @@
-// import { selectAllListings } from '../../reducers/selectors';
+import { selectAllListings } from '../../reducers/selectors';
 import { deleteSpot, requestSingleSpot } from '../../actions/spot_actions';
 import { requestListings } from '../../actions/listings_actions';
 import { connect } from 'react-redux';
 import MyListings from './my_listings';
 
 const mapStateToProps = state => ({
-  // listings: selectAllListings(state),
+  listings: selectAllListings(state),
   loggedIn: !!state.session.currentUser
 });
 
